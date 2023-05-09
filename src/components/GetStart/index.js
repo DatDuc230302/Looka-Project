41e101123dc671bae8f28fa4770c50936b9da1f2
DatDuc230302 <110738474+DatDuc230302@users.noreply.github.com>
2023-05-09 12:07:34 +0700

    Third Commnit

--- src/components/GetStart/index.js
+++ src/components/GetStart/index.js
@@ -1,10 +1,25 @@
 import Button from '../Button';
 import classNames from 'classnames/bind';
 import styles from './GetStart.module.scss';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
 function GetStart({ title, desc, className, classNameInput, fontText }) {
+    const [valueInput, setValueInput] = useState('');
+    const navigate = useNavigate();
+
+    const submitInput = () => {
+        navigate(`/onboarding/${valueInput}`);
+    };
+
+    document.onkeydown = function (e) {
+        if (e.which === 13 && valueInput.length > 0) {
+            navigate(`/onboarding/${valueInput}`);
+        }
+    };
+
     return (
         <div className={`wrapper px-[2.4rem] py-[8.8rem] text-center pt-[134px] ${className}`}>
             <div
@@ -15,12 +30,14 @@ function GetStart({ title, desc, className, classNameInput, fontText }) {
                 {title}
             </div>
             <div className="desc text-[#676e8b] text-[20px] my-[32px] font-normal">{desc}</div>
-            <div className="control md:flex items-center justify-center">
+            <div action="/onboarding" className="control md:flex items-center justify-center">
                 <input
                     className={cx('input', `md:max-w-[424px] ${classNameInput && '!bg-white'}`)}
                     placeholder="Enter your company name"
+                    value={valueInput}
+                    onChange={(e) => setValueInput(e.target.value)}
                 />
-                <Button primary large className={'md:ml-2 w-full md:w-auto mt-3 md:mt-0'}>
+                <Button onClick={submitInput} primary large className={'md:ml-2 w-full md:w-auto mt-3 md:mt-0'}>
                     Get started
                 </Button>
             </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 import Home from '../pages/Home';
 import Onboarding from '../pages/Onboarding';
+import Explore from '../pages/Explore';
 
 const publicRoutes = [
     { path: '/', component: Home },
+    { path: '/onboarding/:key', component: Onboarding, layout: null },
     { path: '/onboarding', component: Onboarding, layout: null },
+    { path: '/explore', component: Explore, layout: null },
 ];
 
 // private routes
