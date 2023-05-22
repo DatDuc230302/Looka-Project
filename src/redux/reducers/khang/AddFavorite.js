f670ca41a1c37b446bf10f427b2619bf3134d2ae
DatDuc230302 <110738474+DatDuc230302@users.noreply.github.com>
2023-05-22 18:12:12 +0700

    Add Favorite

--- src/redux/reducers/khang/AddFavorite.js
+++ src/redux/reducers/khang/AddFavorite.js
@@ -1,11 +1,35 @@
+// export const AddFavorite = (state = [], action) => {
+//     switch (action.type) {
+//         case 'ADDFAVORITE':
+//             const arr = [];
+//             arr.push(action.payload);
+
+//             return [...state, arr];
+//         default:
+//             return state;
+//     }
+// };
+
+let arr = [];
 export const AddFavorite = (state = [], action) => {
     switch (action.type) {
         case 'ADDFAVORITE':
-            const arr = [];
-            arr.push(action.payload);
-
-            return [...state, arr];
+            // arr.push(action.data);
+            // return arr;
+            if (arr.length > 0) {
+                const temp = arr.filter((item) => item.id === action.data.id && item);
+                if (temp.length <= 0) {
+                    arr.push(action.data);
+                } else {
+                    arr = arr.filter((item) => item.id !== action.data.id);
+                }
+            } else {
+                arr.push(action.data);
+            }
+            return [...arr];
         default:
             return state;
     }
 };
+
+export default AddFavorite;
