0942b3f250bd285b62e07fe263d76c1e707c536c
DatDuc230302 <110738474+DatDuc230302@users.noreply.github.com>
2023-05-22 14:33:58 +0700

    Finish

--- src/pages/Onboarding/index.js
+++ src/pages/Onboarding/index.js
@@ -259,7 +259,7 @@ function Onboarding() {
                 case 0:
                     setShowOption(false);
                     setLogic(pick1);
-                    setTimeout(() => setOpa(false), 2000);
+                    setTimeout(() => setOpa(false), 1500);
                     break;
                 case 1:
                     setLogic(pick2.length);
@@ -287,6 +287,7 @@ function Onboarding() {
                 setLogic(pick0.length);
                 break;
             case 2:
+                setTimeout(() => setOpa(false), 1500);
                 setLogic(pick1);
                 break;
             case 3:
@@ -299,6 +300,7 @@ function Onboarding() {
     };
 
     const handleImg = (id) => {
+        setLogic(id);
         dispath(setPick1(id));
     };
 
@@ -332,8 +334,8 @@ function Onboarding() {
     useEffect(() => {
         setLogic(pick0.length);
         setOpa(true);
-        dispath(setPick1(-1));
         if (api1.length > 0 && pick0.length > 0) {
+            dispath(setPick1(-1));
             let arr = api1.filter((item) => item.category === pick0);
             for (var i = 0; i < 16; i++) {
                 let rd = Math.floor(Math.random(arr.length) * arr.length);
@@ -342,10 +344,6 @@ function Onboarding() {
             dispath(setApiPick1(newArr));
         }
     }, [pick0]);
-
-    useEffect(() => {
-        setLogic(pick1);
-    }, [pick1]);
 
     return (
         <div className={cx('wrapper')}>
@@ -407,17 +405,13 @@ function Onboarding() {
                                         {!sticky && <span className={cx('description', 'loadingApi')}></span>}
                                     </div>
                                     <div
-                                        onClick={() => handleContinue()}
                                         className={cx(
                                             'btn-continue',
                                             'loadingApi',
-                                            logic > 0 && 'btn-active',
                                             tablet && 'tablet',
                                             mobile && 'mobile',
                                         )}
-                                    >
-                                        <span className={cx('btn-title')}></span>
-                                    </div>
+                                    ></div>
                                 </div>
                             )}
                         </div>
