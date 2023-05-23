0705d752fe13d44091793dd9b5f4c47c10bfd289
Your Name <khangluong2002@gmail.com>
2023-05-23 08:41:07 +0700

    fix redux

--- src/redux/reducers/khang/AddFavorite.js
+++ src/redux/reducers/khang/AddFavorite.js
@@ -1,31 +1,14 @@
-// export const AddFavorite = (state = [], action) => {
-//     switch (action.type) {
-//         case 'ADDFAVORITE':
-//             const arr = [];
-//             arr.push(action.payload);
-
-//             return [...state, arr];
-//         default:
-//             return state;
-//     }
-// };
-
 let arr = [];
 export const AddFavorite = (state = [], action) => {
     switch (action.type) {
         case 'ADDFAVORITE':
-            // arr.push(action.data);
-            // return arr;
-            if (arr.length > 0) {
-                const temp = arr.filter((item) => item.id === action.data.id && item);
-                if (temp.length <= 0) {
-                    arr.push(action.data);
-                } else {
-                    arr = arr.filter((item) => item.id !== action.data.id);
-                }
+            const objData = { ...action.data };
+            if (arr.filter((e) => e.id === action.data.id).length > 0) {
+                arr = arr.filter((item) => item.id !== action.data.id);
             } else {
-                arr.push(action.data);
+                arr.push(objData);
             }
+
             return [...arr];
         default:
             return state;
